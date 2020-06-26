ddee7aa7995188d8157058691bbaffe8b15299c6
Drew Tschetter <drew.tschetter@gmail.com>
2020-06-26 13:50:16 -0400

    Moved bot token and owner ID to config file. Added mission calculation functions.

--- cogs/calcs.py
+++ cogs/calcs.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from datetime import datetime as d
+import datetime as dt
 import numpy as np
 import re
 
@@ -16,12 +16,6 @@ class Calculation(commands.Cog):
         usage='<rad-Z 1> <rad-Z 2>'
     )
     async def distance(self, ctx, coord1, coord2):
-
-        # msg = ctx.message.content
-        # prefix_used = ctx.prefix
-        # alias_used = ctx.invoked_with
-        # text = msg[len(prefix_used) + len(alias_used):]
-        #
         if coord1 == '':
             await ctx.send(content="You need to list the first system's coordinates")
             pass
@@ -35,20 +29,70 @@ class Calculation(commands.Cog):
             await ctx.send(content="You must enter the coordinates in the #####-##### format")
             pass
         else:
-            # await ctx.send('You sent {} and {} as the two systems to calculate distance.'.format(coord1, coord2))
             r1 = int(re.search(r"^\d{5}", coord1).group(0))
             z1 = int(re.search(r"\d{5}$", coord1).group(0))
             r2 = int(re.search(r"^\d{5}", coord2).group(0))
             z2 = int(re.search(r"\d{5}$", coord2).group(0))
-            # await ctx.send("System 1's coords are R: {} and Z: {}"
-            #                "\nSystem 2's coords are R: {} and Z: {}".format(r1, z1, r2, z2))
             dist = np.around(np.sqrt((np.square(z1) + np.square(z2)) -
                                      2 * (z1 * z2 * (np.cos(r1 / 10000 - r2 / 10000)))) / 10, 1)
             await ctx.send("The distance between {} and {} is {}JU".format(coord1, coord2, dist))
+            pass
+        return
 
+    @commands.command(
+        name='missions',
+        description='Command to calculate mission times to a system in "<days>, hh:mm:ss" format, '
+                    'along with associated costs',
+        aliases=['m'],
+        usage='<rad-Z> <# of stars+planets> <Subroutine Scanner upgrade (T/F)>'
+    )
+    async def mission(self, ctx, coord, bodies=0, upgrade=False):
+        if coord == '':
+            await ctx.send(content="You need to list the system's coordinates")
+            pass
+        elif not re.search(r"\d{5}-\d{5}", coord):
+            await ctx.send(content="You must enter the coordinates in the #####-##### format")
+            pass
+        elif not isinstance(bodies, int):
+            await ctx.send(content="You must enter the # of stars and planets as an integer")
+            pass
+        else:
+            # r = int(re.search(r"^\d{5}", coord).group(0))
+            z = int(re.search(r"\d{5}$", coord).group(0))
+            z_hours = np.floor(z/10)
+            probe_time = dt.timedelta(hours=int(z_hours + np.floor(bodies/2)),
+                                      minutes=int(((z/10) - z_hours) * 60 + (bodies % 2) * 30))
+            # probe_hours = int(z_hours + b_hours)
+            # probe_mins = int(((z/10) - z_hours) * 60 + (bodies % 2) * 30)
+            pioneer_time = dt.timedelta(hours=int(z_hours + 6), minutes=int(((z/10) - z_hours) * 60))
+            # pioneer_hours = int(z_hours + 6)
+            # pioneer_mins = int(((z/10) - z_hours) * 60)
+            pioneer_xe = np.ceil(z/10) * 0.1
+            if upgrade:
+                pioneer_w = 2
+            else:
+                pioneer_w = 3
+            fs_time = dt.timedelta(hours=int(z_hours), minutes=int(((z/10) - z_hours) * 60))
+            # fs_hours = int(z_hours)
+            # fs_mins = int(((z/10) - z_hours) * 60)
+            fs_xe = (np.ceil(z/10) * 0.2) + 1
+            sos_time = dt.timedelta(hours=int(z_hours + 6), minutes=int(((z/10) - z_hours) * 60))
+            # sos_hours = int(z_hours + 6)
+            # sos_mins = int(((z/10) - z_hours) * 60)
+            sos_xe = (np.ceil(z/10) * 0.3) + 3
+            await ctx.send("A probe to {} will take {} and cost 3 Plasteel\n"
+                           "A pioneer to {} will take {} and cost {} Xe, {} Water, 2 Plasteel\n"
+                           "A fuel station to {} will take {} and cost {} Xe, 3 Water, 3 Ore, 3 Plasteel\n"
+                           "A small orbital station to {} will take {} and cost {} Xe, 3 Water, 3 Ore, "
+                           "1 Plasteel, 10 Licenses"
+                           .format(coord, str(probe_time),
+                                   coord, str(pioneer_time), pioneer_xe, pioneer_w,
+                                   coord, str(fs_time), fs_xe,
+                                   coord, str(sos_time), sos_xe))
             pass
 
         return
+
 
     # @commands.command(
     #     name='',
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
+import json
 import discord
 from discord.ext import commands
+
+with open('config.json') as config_file:
+    secrets = json.load(config_file)
 
 
 def get_prefix(bot_client, message):
@@ -11,11 +14,17 @@ def get_prefix(bot_client, message):
 bot = commands.Bot(
     command_prefix=get_prefix,
     description='Bot to help Exogen players make calculations',
-    owner_id=os.environ.get("OWNER_ID"),
+    owner_id=secrets['owner_id'],
     case_insensitive=True
 )
 
 cogs = ['cogs.calcs']
+
+
+@bot.command(name='eval')
+@commands.is_owner()
+async def _eval(ctx, *, code):
+    await ctx.send(eval(code))
 
 
 @bot.event
@@ -29,5 +38,4 @@ async def on_ready():
         bot.load_extension(cog)
     return
 
-token = os.environ.get("DISCORD_TOKEN")
-bot.run(token)
+bot.run(secrets['token'])
