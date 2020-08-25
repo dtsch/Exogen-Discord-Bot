5c1238f139d9e9881f0140dc4c698b7aea7e5b0d
drewt <drew.tschetter@gmail.com>
2020-08-25 16:57:08 -0400

    Added mission commission calculations.

--- cogs/calcs.py
+++ cogs/calcs.py
@@ -19,17 +19,13 @@ class Calculation(commands.Cog):
     # function that command runs
     async def distance(self, ctx, coord1, coord2):
         if coord1 == '':  # checking for blank arg
-            await ctx.send(content="`You need to list the first system's coordinates.`")
-            pass
+            await ctx.send(content="`***ERROR:*** You need to list the first system's coordinates.`")
         elif coord2 == '':  # checking for blank arg
-            await ctx.send(content="`You need to list the second system's coordinates.`")
-            pass
+            await ctx.send(content="`***ERROR:*** You need to list the second system's coordinates.`")
         elif not re.search(r"\d{5}-\d{5}", coord1):  # checking for arg format
-            await ctx.send(content="`You must enter the coordinates in the #####-##### format.`")
-            pass
+            await ctx.send(content="`***ERROR:*** You must enter the coordinates in the #####-##### format.`")
         elif not re.search(r"\d{5}-\d{5}", coord2):  # checking for arg format
-            await ctx.send(content="`You must enter the coordinates in the #####-##### format.`")
-            pass
+            await ctx.send(content="`***ERROR:*** You must enter the coordinates in the #####-##### format.`")
         else:  # if passes checks, parses args for calculation
             r1 = int(re.search(r"^\d{5}", coord1).group(0))
             z1 = int(re.search(r"\d{5}$", coord1).group(0))
@@ -38,7 +34,6 @@ class Calculation(commands.Cog):
             dist = np.around(np.sqrt((np.square(z1) + np.square(z2)) -
                                      2 * (z1 * z2 * (np.cos((r1 - r2) / 10000)))) / 10, 1)
             await ctx.send("`The distance between {} and {} is {}JU.`".format(coord1, coord2, dist))
-            pass
         return
 
     # command info
@@ -46,23 +41,20 @@ class Calculation(commands.Cog):
         name='missions',
         description='Command to calculate mission times to a system in "<days>, hh:mm:ss" format, '
                     'along with associated costs',
-        aliases=['m', 'mz', 'm1'],
-        usage='<rad-Z> <# of stars+planets> <Mission (All, Probe, PB, Pioneer, PN, Fuel Station, FS, '
-              'Small Orbital Station, SOS, Science Vessel, SV, Mining Operation, MO> '
+        aliases=['mission', 'm', 'mz', 'm1'],
+        usage='<rad-Z> <# of stars+planets> <Mission (All, Prob/PB, Pioneer/PN, Fuel Station/FS, '
+              'Small Orbital Station/SOS, Science Vessel/SV, Mining Operation/MO)> '
               '<Subroutine Scanner upgrade (True/False)> <Augmented Workforce upgrade (True/False)> '
               'Subroutine Mining Drones upgrade (True/False)'
     )
     # function that command runs
     async def mission(self, ctx, coord, bodies=1, mission="All", ss=False, aw=False, smd=False):
         if coord == '':  # checking for blank arg
-            await ctx.send(content="`You need to list the system's coordinates`")
-            pass
+            await ctx.send(content="`***ERROR:*** You need to list the system's coordinates`")
         elif not re.search(r"\d{5}-\d{5}", coord):  # checking for arg format
-            await ctx.send(content="`You must enter the coordinates in the #####-##### format`")
-            pass
+            await ctx.send(content="`***ERROR:*** You must enter the coordinates in the #####-##### format`")
         elif not isinstance(bodies, int):  # checking for arg format
-            await ctx.send(content="`**ERROR:** You must enter the # of stars and planets as an integer`")
-            pass
+            await ctx.send(content="`***ERROR:*** You must enter the # of stars and planets as an integer`")
         else:  # if passes checks, parses args for calculation
             # r = int(re.search(r"^\d{5}", coord).group(0))
             z = int(re.search(r"\d{5}$", coord).group(0))
@@ -129,25 +121,92 @@ class Calculation(commands.Cog):
                 .format(coord, str(mo_time), mo_xe, mo_ore))
         else:
             await ctx.send(content="```***ERROR*** You must enter a viable mission type, abbreviation, or 'All'.```")
+        return
 
+    # command info
+    @commands.command(
+        name='commission',
+        description='Command to calculate commission for Probes, Pioneers, and Science Vessels to systems and planets, '
+                    'given their rad-Z coordinates and any applicable upgrades.',
+        aliases=['c', 'cz', 'c1'],
+        usage='<rad-Z> <# of stars+planets> <Mission (Probe/PB, Pioneer/PN, Science Vessel/SV)> '
+              '<Astro-cartography Database upgrade (True/False)> <Printable Pocket Labs upgrade (True/False)> '
+              '<Quantum Spectrometer upgrade (True/False)>'
+    )
+    # function that command runs
+    async def commission(self, ctx, coord, mission, explored=True, ad=True, ppl=False, qs=False, w_stars=0, o_stars=0,
+                       rare_stars=0, other_stars=1, rare_planets=0, planets=0, planet_type="Uninhabitable"):
+        if coord == '':  # checking for blank arg
+            await ctx.send(content="`***ERROR:*** You need to list the system's/planet's coordinates.`")
+        elif not re.search(r"\d{5}-\d{5}", coord):  # checking for arg format
+            await ctx.send(content="`***ERROR:*** You must enter the coordinates in the #####-##### format.`")
+        elif not isinstance(w_stars, int) or not isinstance(o_stars, int) or not isinstance(rare_stars, int)\
+                or not isinstance(other_stars, int) or not isinstance(rare_planets, int) \
+                or not isinstance(planets, int):  # checking for arg format
+            await ctx.send(content="`***ERROR:*** You must enter the # of stars and planets as an integer`")
+        else:  # if passes checks, parses args for calculation
+            if explored:
+                first = 1
+            else:
+                first = 2
 
-        # await ctx.send("```A probe to {} will take {} and cost 3 Plasteel.\n"
-        #                "A pioneer to {} will take {} and cost {} Xe, {} Water, 2 Plasteel.\n"
-        #                "A fuel station to {} will take {} and cost {} Xe, 3 Water, 3 Ore, 3 Plasteel.\n"
-        #                "A small orbital station to {} will take {} and cost {} Xe, 3 Water, 3 Ore, "
-        #                "1 Plasteel, 10 Licenses.\n"
-        #                "A science vessel to {} will take {} and cost {} Xe, 3 Water, 1 Ore, 10 Plasteel, "
-        #                "10 Licenses.\n"
-        #                "A mining operation to {} will take {} and cost {} Xe, 5 Water, 5 Ore, 5 Plasteel, "
-        #                "5 Licenses.```"
-        #                .format(coord, str(probe_time),
-        #                        coord, str(pioneer_time), pioneer_xe, pioneer_w,
-        #                        coord, str(fs_time), fs_xe,
-        #                        coord, str(sos_time), sos_xe,
-        #                        coord, str(sv_time), sv_xe,
-        #                        coord, str(mo_time), mo_xe))
-            pass
+            if mission == "Probe" or mission == "PB":
+                if ad:
+                    upgrade = 0.1 * (w_stars + o_stars + rare_stars + other_stars + rare_planets + planets)
+                else:
+                    upgrade = 0
+                commission = (((w_stars * 10) + (o_stars * 2) + (rare_stars * 10) + (other_stars * 0.1) +
+                               (rare_planets * 10) + (planets * 0.1)) + upgrade) * first
+                await ctx.send("```Sending a Probe to {} will return a commission of {} Licenses.```"
+                               .format(coord, commission))
+            elif mission == "Pioneer" or mission == "PN":
+                p_types = {"Craters World": 1
+                           ,"Desert Craters World": 2
+                           ,"Desert Dunes World": 2
+                           ,"Desert Metal-rich World": 2
+                           ,"Desert Rocky World": 2
+                           ,"Desert Volcanic World": 2
+                           ,"Desert World": 1
+                           ,"Dunes Craters World": 2
+                           ,"Dunes Metal-rich World": 2
+                           ,"Dunes Rocky World": 2
+                           ,"Dunes Volcanic World": 2
+                           ,"Dunes World": 1
+                           ,"Metal-rich Craters World": 2
+                           ,"Metal-rich Volcanic World": 2
+                           ,"Metal-rich World": 1
+                           ,"Rocky Craters World": 2
+                           ,"Rocky Metal-rich World": 2
+                           ,"Rocky Volcanic World": 2
+                           ,"Rocky World": 1
+                           ,"Volcanic Craters World": 2
+                           ,"Volcanic World": 1
+                           ,"Rare Planetary Body": 10
+                           ,"Destroyed Planet": 0
+                           ,"Earth-like World": 0
+                           ,"Water World": 0
+                           ,"Moon": 0
+                           ,"Comet": 0
+                           ,"Uninhabitable": 0
+                           }
 
+                if ppl:
+                    upgrade = 2
+                else:
+                    upgrade = 1
+                commission = p_types[planet_type] * first * ppl
+                await ctx.send("```Sending Pioneers to {} will return a commission of {} Licenses.```"
+                               .format(coord, commission))
+
+            elif mission == "Science Vessel" or mission == "SV":
+                if qs:
+                    commission = 60
+                else:
+                    commission = 50
+                await ctx.send("```Sending a Science Vessel to {} will return a commission of {} Licenses.```"
+                               .format(coord, commission))
+            else:
+                await ctx.send(content="```***ERROR*** You must enter a viable mission type or abbreviation.```")
         return
 
 
